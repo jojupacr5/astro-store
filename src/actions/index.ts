7c69b9e3c0142a9b53877a059d309dd2e65ca6ac
jojupacr5 <joseph.paucar@gmail.com>
2025-05-26 23:30:01 -0500

    Añadiendo la pagina del Producto y el diseño del mismo

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,4 +1,5 @@
 import { loginUser, logout, registerUser } from './auth';
+import { getProductBySlug } from './products/get-product-by-slug.action';
 import { getProductsByPage } from './products/get-products-by-page.action';
 
 export const server = {
@@ -10,5 +11,6 @@ export const server = {
   registerUser,
 
   // Products
-  getProductsByPage
+  getProductsByPage,
+  getProductBySlug
 };
